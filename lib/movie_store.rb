985181d451861f85d393635c2fd3a11aa663f137
ottagit <ottachris13@yahoo.com>
2016-10-11 18:37:36 +0300

    a class to manage yaml store

---
+++ lib/movie_store.rb
@@ -0,0 +1,20 @@
+# require 'movie'
+require 'YAML::Store'
+
+class MovieStore
+
+  def initialize(file_name)
+   @store = YAML::Store.new('file_name') #create store that reads/writes file_name
+  end
+
+  def saveMovie(movie) # save a movie to the store
+   @store.transaction do # must be in a transaction
+   unless movie.id
+    highest_id = @store.roots.max || 0
+    movie.id = highest_id + 1
+   end
+   @store[movie.id] = movie
+   end
+  end
+
+end
