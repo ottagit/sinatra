8cc67435ffe9d27842ed167f6b6460de0be5df91
ottagit <ottachris13@yahoo.com>
2016-10-11 12:47:31 +0300

    just some code review

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ get('/movies') do
   erb :index
 end
 
-get('/movies/new')do
+get('/movies/new') do
   erb :new
 end
 
-post('/movies/create')do
+post('/movies/create') do
   "Received: #{params.inspect}"
   @movie = Movie.new
   @movie.title = params['title']
