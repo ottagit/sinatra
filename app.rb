a51af73e92bfa9f9067019d76ed91452d09c7efe
ottagit <ottachris13@yahoo.com>
2016-10-13 11:53:28 +0300

    use all to retrieve all movie objects

--- app.rb
+++ app.rb
@@ -6,13 +6,7 @@ store = MovieStore.new('movies.yml')
 
 
 get('/movies') do
-  @movies = []
-  @movies[0] = Movie.new
-  @movies[0].title = "Jaws"
-  @movies[1] = Movie.new
-  @movies[1].title = "Taken"
-  @movies[2] = Movie.new
-  @movies[2].title = "Taken 2" 
+   @movies = @store.all 
   erb :index
 end
 
